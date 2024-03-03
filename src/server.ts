262316cd7906d7566327c529fadac3c7252e4304
makarovas <makarofalex@gmail.com>
2024-03-03 20:00:51 +0200

    feat: api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,68 @@
-// server.ts
 import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
+import mongoose, { ConnectOptions } from 'mongoose';
+
 import TrackingDataModel from './TrackingDataModel';
 import { trackEvent } from './googleAnalytics';
 
+const MONGODB_URI = process.env.MONGODB_URI!;
+console.log(MONGODB_URI, 9999);
 const PORT = 8001;
 
+mongoose.connect(MONGODB_URI, {} as ConnectOptions);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected successfully');
+});
+
+const newTrackingData = new TrackingDataModel({
+  userId: 'user123',
+  eventType: 'click',
+  eventData: { action: 'button_click', page: 'homepage' },
+  timestamp: new Date(),
+});
+
+const newTrackingData2 = new TrackingDataModel({
+  userId: 'user122',
+  eventType: 'click',
+  eventData: { action: 'button_click', page: 'homepage' },
+  timestamp: new Date(),
+});
+
+const newTrackingData3 = new TrackingDataModel({
+  userId: 'user132',
+  eventType: 'click',
+  eventData: { action: 'button_click', page: 'homepage' },
+  timestamp: new Date(),
+});
+
+newTrackingData
+  .save()
+  .then((result) => {
+    console.log('New tracking data saved successfully:', result);
+  })
+  .catch((error) => {
+    console.log('Error saving tracking data:', error);
+  });
+
+newTrackingData2
+  .save()
+  .then((result) => {
+    console.log('New tracking data saved successfully:', result);
+  })
+  .catch((error) => {
+    console.log('Error saving tracking data:', error);
+  });
+
+newTrackingData3
+  .save()
+  .then((result) => {
+    console.log('New tracking data saved successfully:', result);
+  })
+  .catch((error) => {
+    console.log('Error saving tracking data:', error);
+  });
 const app = express();
 
 app.use(express.json());
